1e6b20a3e4be627b33660d820697037ba1a0027a
Joan M. Tovar <joan.tovar@correounivalle.edu.co>
2020-06-03 00:47:03 -0500

    Created Book Component which presents the information of each book

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class Bookshelf extends Component {
     render(){
@@ -9,24 +10,7 @@ class Bookshelf extends Component {
                 <div className="bookshelf-books">
                 <ol className="books-grid">
                     {books.map((book) => (
-                    <li key={book.id}>
-                        <div className="book">
-                        <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: '{book.imageLinks.thumbnail}' }}></div>
-                            <div className="book-shelf-changer">
-                            <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                            </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors.join(', ')}</div>
-                        </div>
-                    </li>
+                        <Book book={book} />
                     ))}
                 </ol>
                 </div>
